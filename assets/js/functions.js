576ad2839748f6768bd529d486e53233a22b33c9
Nuno Cardoso <nc@ncdesign.pt>
2015-11-22 17:46:50 +0900

    continue the text toggle for about me

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,36 @@
+
 $( document ).ready(function() {
+    // var hero = document.getElementsByClassName('hero');
+    // console.log(hero);
+    // var music = document.getElementById('music');
+    // music.style.display="none";
+    $('.hero').css('display','none');
+    $('#default').css('display','block');
+    // document.getElementById("music_logo").onclick = function() {
+    //   alert('Item selected');
+    // };
+    $(".icon-music").click(function() {
+          $('path').css('fill','#6597CA');
+          $('.hero').css('display','none');
+          $('#music').css('display','block');
+          $('.icon-music path').css('fill','#022849');
+              // document.getElementById('default').style.display="none";
+              // document.getElementById('music').style.display="block";
+              // $('.icon-music path').animate({'fill':'red'}, 'slow'); SVG cant be animated with Jquery
+    });
+    $(".avatar").click(function() {
+          $('.hero').css('display','none');
+          $('#default').css('display','block');
+          $('path').css('fill','#6597CA');
+    });
+    $(".icon-positive").click(function() {
+          $('path').css('fill','#6597CA');
+          $('.hero').css('display','none');
+          $('#positive').css('display','block');
+          $('.icon-positive path').css('fill','#022849');
+    });
+});
 
-  // Get started!
-
+$( window ).load(function() {
+    console.log( "window loaded" );
 });
